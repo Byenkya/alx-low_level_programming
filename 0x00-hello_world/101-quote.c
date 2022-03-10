a37dd18d4b85d820f7fa172652aced77b27c0085
Byenkya <byenkyajoshua1997@gmail.com>
2022-03-10 04:29:47 -0800

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  * Return: Always 1 (Success)
  */
 int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful"
-			"\" - Dora Korpar, 2015-10-19", 59);
+{	char *msg = "and that piece of art is userful\" - Dora Korpar, 2015-10-19";
+	write(STDOUT_FILENO, msg, 59);
 	return (1);
 }
