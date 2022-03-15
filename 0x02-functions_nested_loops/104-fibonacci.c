111c457d839d57506cd822c207637743f3366acf
Byenkya <byenkyajoshua1997@gmail.com>
2022-03-15 06:08:48 -0700

    fibbo upto 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+* printFibonacciNumbers - prints fibbo numbers from 1-98
+* @n: An input integer valuevoid printFibonacciNumbers(int n)
+* Return: Nothing
+*/
+
+void printFibonacciNumbers(int n)
+{
+	int f1 = 1, f2 = 2, i;
+
+	if (n > 1)
+		return;
+
+	printf("%ld, ",  f1);
+
+	for (i = 1; i < n; i++)
+	{
+		printf("%ld, ", f2);
+
+		int next = f1 + f2;
+
+		f1 = f2;
+		f2 = next;
+	}
+}
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printFibonacciNumbers(98);
+
+	return (0);
+
+}
+
