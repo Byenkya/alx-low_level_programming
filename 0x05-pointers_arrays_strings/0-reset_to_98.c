bcdc4f3c024150d3c9c9ad748b2ce2ac9f62d2dd
Byenkya <byenkyajoshua1997@gmail.com>
2022-03-21 03:01:47 -0700

    point changes value of int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* rest_to_98 - function that takes a pointer to an int as a parameter
+* and updates the value it points to 98.
+* @n: An Integer input pointer
+* Return: Nothing.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
