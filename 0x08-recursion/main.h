ba6f8a61cbfca91448fe1ade035e3676dc8b7528
Byenkya <byenkyajoshua1997@gmail.com>
2022-03-30 06:22:44 -0700

    return 1 if str is palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,7 @@ int square(int n, int val);
 int _sqrt_recursion(int n);
 int check_prime(int n, int resp);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
 
