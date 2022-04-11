b3894d278a515d1de6c4e2fc2e28e05cb7e236f1
Byenkya <byenkyajoshua1997@gmail.com>
2022-04-11 10:24:45 -0700

    function like macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
-#include <stdio.h>
-/**
-* main - prints the name of the file.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
